2cce9504a7c806b6ff7dae62812268e77deb287c
MSBivens <mikebiv95@gmail.com>
2022-04-23 14:26:23 -0500

    Updated controller imports

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,5 +1,10 @@
 from flask_app import app
 from flask import redirect,render_template,request
+from flask_app.models.user import User
+from flask_app.models.door import Door
+from flask_app.models.wheel import Wheel
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 #create post route for voting 
 
---
+++ flask_app/controllers/wheels.py
@@ -0,0 +1,6 @@
+from flask_app import app
+from flask import redirect,render_template,request
+from flask_app.models.user import User
+from flask_app.models.door import Door
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
